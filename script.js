443d0d2921f194ca86ed492e72c1706c985359c2
Abdel <abdelbarka96@gmail.com>
2024-08-10 21:13:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const shareIcon = document.querySelector("#shareIcon");
-const shareIconActive = document.querySelector("#shareIconActive");
+const activeIcon = document.querySelector("#activeIcon");
+const sectionContent = document.querySelector("#sectionContent");
 const shareSection = document.querySelector("#shareSection");
+const imgPathRegex = /\/images\/icon-share\.svg$/;
 
 shareIcon.addEventListener('click',(event)=>{
-    shareSection.style.display = 'flex';
-});
+    sectionContent.classList.toggle('activePosition');
+    shareSection.classList.toggle('active');
+    shareIcon.classList.toggle('activeShareButton');
 
-shareIconActive.addEventListener('click',(event)=>{
-    shareSection.style.display = 'none';
+    if(imgPathRegex.test(activeIcon.src)){
+        activeIcon.src = '/images/icon-share-active.svg';
+    }else{
+        activeIcon.src = '/images/icon-share.svg';
+    }
+
 });
