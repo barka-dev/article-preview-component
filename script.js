d088c2df58e2aa60c636f91017d3e5bc906c8256
Abdel <abdelbarka96@gmail.com>
2024-08-11 11:06:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const sectionContent = document.querySelector("#sectionContent");
 const shareSection = document.querySelector("#shareSection");
 const imgPathRegex = /\/images\/icon-share\.svg$/;
 
+
+
 shareIcon.addEventListener('click',(event)=>{
     sectionContent.classList.toggle('activePosition');
     shareSection.classList.toggle('active');
     shareIcon.classList.toggle('activeShareButton');
+
+    if (window.innerWidth >= 768) {
+        sectionContent.classList.remove('activePosition');
+    }
 
     if(imgPathRegex.test(activeIcon.src)){
         activeIcon.src = '/images/icon-share-active.svg';
     }else{
         activeIcon.src = '/images/icon-share.svg';
     }
+
+    
 
 });
